482dc371f5844fca08a6b8304eff9b15459e54cc
KangSDhi <kangteknisi@gmail.com>
2022-11-16 12:06:52 +0700

    first commit

---
+++ src/main/kotlin/programmer/zaman/now/kotlin/restful/auth/ApiKeyInterceptor.kt
@@ -0,0 +1,33 @@
+package programmer.zaman.now.kotlin.restful.auth
+
+import org.springframework.stereotype.Component
+import org.springframework.ui.ModelMap
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.context.request.WebRequestInterceptor
+import programmer.zaman.now.kotlin.restful.error.UnauthorizedException
+import programmer.zaman.now.kotlin.restful.repository.ApiKeyRepository
+import java.lang.Exception
+
+@Component
+class ApiKeyInterceptor(val apiKeyRepository: ApiKeyRepository) : WebRequestInterceptor {
+    override fun preHandle(request: WebRequest) {
+        val apikey = request.getHeader("X-Api-Key")
+        if (apikey == null){
+            throw UnauthorizedException()
+        }
+
+        if (!apiKeyRepository.existsById(apikey)){
+            throw UnauthorizedException()
+        }
+
+        // valid
+    }
+
+    override fun postHandle(request: WebRequest, model: ModelMap?) {
+        // nothing
+    }
+
+    override fun afterCompletion(request: WebRequest, ex: Exception?) {
+        // nothing
+    }
+}
---
+++ src/main/kotlin/programmer/zaman/now/kotlin/restful/model/ListProductRequest.kt
@@ -0,0 +1,7 @@
+package programmer.zaman.now.kotlin.restful.model
+
+data class ListProductRequest(
+        val page: Int,
+
+        val size: Int
+)
